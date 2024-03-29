bd7ac58859f17c7f88a1dfc17375958c5ab53a32
LUCAS <lucasazevedo.p77@gmail.com>
2024-03-29 00:56:38 -0300

    Input de URL automatizada

--- run_all_tests.py
+++ run_all_tests.py
@@ -3,7 +3,11 @@ import pathlib
 import sys
 from io import StringIO
 
-def run_test_script(script_path):
+def run_test_script(script_path, url_base):
+    # Configurando o ambiente do script com a URL base
+    original_argv = sys.argv  # Corrigido aqui
+    sys.argv = [str(script_path), url_base]  # Simula argumentos de linha de comando para o script, incluindo a URL base
+
     # Capturar a saída padrão
     original_stdout = sys.stdout
     sys.stdout = StringIO()
@@ -16,14 +20,18 @@ def run_test_script(script_path):
     # Capturar a saída do script
     result = sys.stdout.getvalue()
 
-    # Restaurar a saída padrão
+    # Restaurar a saída padrão e os argumentos originais
     sys.stdout = original_stdout
+    sys.argv = original_argv  # Restaura os argumentos originais
 
     return result
 
 def main():
+    # Solicitar a URL base do usuário
+    url_base = input("Por favor, insira a URL base: ")
+
     # Caminho para o diretório contendo os scripts de teste
-    test_scripts_dir = pathlib.Path('./payloads')  # Assume que os scripts estão no diretório atual
+    test_scripts_dir = pathlib.Path('.')  # Atualize este caminho conforme necessário
 
     # Lista para armazenar os resultados dos testes
     test_results = []
@@ -32,7 +40,7 @@ def main():
     for test_script in test_scripts_dir.glob('*.py'):
         if test_script.name != 'run_all_tests.py':  # Ignorar o script principal
             print(f'Executando: {test_script.name}')
-            result = run_test_script(test_script)
+            result = run_test_script(script_path=test_script, url_base=url_base)
             test_results.append((test_script.name, result))
 
     # Exibindo os resultados
